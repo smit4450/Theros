24883da4e229c1d4428506ba3dc4dbd7388ba809
smit4450 <117391305+smit4450@users.noreply.github.com>
2026-02-10 10:40:32 -0500

    update analytics configuration and change base URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,11 +12,9 @@ const config: QuartzConfig = {
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "smit4450.github.io/Theros",
     ignorePatterns: ["private", "templates", ".obsidian", "Compendium"],
     defaultDateType: "modified",
     theme: {
