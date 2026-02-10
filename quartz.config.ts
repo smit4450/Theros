1c8683f02f196cdf187fd79fedf618ab082154de
smit4450 <117391305+smit4450@users.noreply.github.com>
2026-02-10 11:08:57 -0500

    re-included Compendium

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: null,
     locale: "en-US",
     baseUrl: "smit4450.github.io/Theros",
-    ignorePatterns: ["private", "templates", ".obsidian", "Compendium"],
+    ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "local",
