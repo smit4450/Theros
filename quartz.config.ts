e4537b195ae3d9e1f50f673495209c03c93bdd58
smit4450 <117391305+smit4450@users.noreply.github.com>
2026-02-10 11:04:58 -0500

    replaced spaces in font file names and reverted base url path

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "smit4450.github.io",
+    baseUrl: "smit4450.github.io/Theros",
     ignorePatterns: ["private", "templates", ".obsidian", "Compendium"],
     defaultDateType: "modified",
     theme: {
