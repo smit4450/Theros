8c3b07742100c70f1bf6a282bab438ecfe46640e
smit4450 <117391305+smit4450@users.noreply.github.com>
2026-02-15 11:03:03 -0500

    disabled graph

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,7 +42,7 @@ export const defaultContentPageLayout: PageLayout = {
     FolderNoteExplorer(),
   ],
   right: [
-    Component.Graph(),
+    //Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
